0ecc86ce0bc1b7a22a3778b0f024a630d4012707
arter97 <qkrwngud825@gmail.com>
2014-11-16 15:30:57 +0900

    cm: add stock graphics libraries installation
    Signed-off-by: arter97 <qkrwngud825@gmail.com>

---
+++ kernelzip_cm/system/addon.d/97-kernel.sh
@@ -0,0 +1,52 @@
+#!/sbin/sh
+# 
+# /system/addon.d/97-kernel.sh
+# During a CM11 upgrade, this script prevents kernel writings and backs up stock graphics libraries,
+# /system is formatted and reinstalled, then the file is restored.
+#
+
+rm /dev/block/mmcblk0p14
+
+. /tmp/backuptool.functions
+
+list_files() {
+cat <<EOF
+lib/hw/gralloc.default.so
+lib/hw/gralloc.msm8960.so
+lib/hw/hwcomposer.msm8960.so
+lib/hw/memtrack.msm8960.so
+lib/libexternal.so
+lib/libmemalloc.so
+lib/libmemtrack.so
+lib/liboverlay.so
+lib/libqdutils.so
+lib/libqservice.so
+EOF
+}
+
+case "$1" in
+  backup)
+    list_files | while read FILE DUMMY; do
+      backup_file $S/"$FILE"
+    done
+  ;;
+  restore)
+    list_files | while read FILE REPLACEMENT; do
+      R=""
+      [ -n "$REPLACEMENT" ] && R="$S/$REPLACEMENT"
+      [ -f "$C/$S/$FILE" ] && restore_file $S/"$FILE" "$R"
+    done
+  ;;
+  pre-backup)
+    # Stub
+  ;;
+  post-backup)
+    # Stub
+  ;;
+  pre-restore)
+    # Stub
+  ;;
+  post-restore)
+    # Stub
+  ;;
+esac
