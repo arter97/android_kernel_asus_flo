29626946b198553217ba7968de952e192120cf56
arter97 <qkrwngud825@gmail.com>
2014-11-21 20:09:29 +0900

    build_master: suppress error log
    Signed-off-by: arter97 <qkrwngud825@gmail.com>

--- build_master.sh
+++ build_master.sh
@@ -30,7 +30,7 @@ if [ -e cm.img ] ; then
 fi
 
 if [ -e recovery.img ] ; then
-	rm arter97-recovery-"$(cat version)"-philz_touch_"$(cat version_recovery | awk '{print $1}')".zip
+	rm arter97-recovery-"$(cat version)"-philz_touch_"$(cat version_recovery | awk '{print $1}')".zip 2>/dev/null
 	cp recovery.img recoveryzip/
 	cd recoveryzip/
 	sed -i -e s/PHILZ_VERSION/$(cat ../version_recovery | awk '{print $1}')/g -e s/CWM_VERSION/$(cat ../version_recovery | awk '{print $2 }')/g META-INF/com/google/android/updater-script
